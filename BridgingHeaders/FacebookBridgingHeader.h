e7c49ea228dce1db3f4eeb0bc7ee79f4b1e7a344
Metex <metex@ufoh.com>
2016-05-06 14:26:13 -0700

    Facebook Bridge
    Bridge File for Facebook headers

---
+++ BridgingHeaders/FacebookBridgingHeader.h
@@ -0,0 +1,17 @@
+//
+//  FacebookBridgingHeader.h
+//  EduChoice
+//
+//  Created by Elliot Grafil on 5/5/16.
+//  Copyright © 2016 Elliot Grafil. All rights reserved.
+//
+
+//http://www.brianjcoleman.com/tutorial-how-to-use-login-in-facebook-sdk-4-0-for-swift/
+#ifndef FacebookBridgingHeader_h
+#define FacebookBridgingHeader_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* FacebookBridgingHeader_h */
